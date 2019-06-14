2c75ca59876f55e871929eaa439b1e57e093d8b5
Megan Wu <meganwu1994@gmail.com>
2019-06-14 15:40:58 -0500

    nothing

--- logic.js
+++ logic.js
@@ -30,13 +30,15 @@ $("#search-btn").on("click", function(event) {
 
 // Travel API
 function displayTrip() {
-    var tripKey = "";
-  	var tripURL = "";
+    var tripKey = "AIzaSyC5AxjSx3IHdXy5Jb5iq-2Fx-ddPaNLRjs";
+  	var tripURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=austin&inputtype=textquery&fields=photos,name,opening_hours,geometry&key=" + tripKey;
   
   	$.ajax({
       url: tripURL,
-      method: GET
-    })
+      method: "GET"
+    }).then(function(response){
+        console.log(response);
+      });
   
 		
           
@@ -44,4 +46,4 @@ function displayTrip() {
 
 
 // On click a selected movie...
-$(document).on("click", ".movieBtn", displayTrip);
+$(document).on("click", "#movieBtn", displayTrip);
