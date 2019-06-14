6260e7c35dd80abdb83161197c8dc86225c2e767
mcollums <collumsmichelle@gmail.com>
2019-06-14 12:55:02 -0500

    Started working on the OMDB Api Ajax Call

--- logic.js
+++ logic.js
@@ -1,1 +1,46 @@
 // Comment everything you code!!
+
+var keyword = "";
+
+// On click search button...
+$("#search-btn").on("click", function {
+    //Gets the keyword from the search input box
+    keyword = $("#search-input").val();
+
+		//OMDB Api Key
+    var movieKey = "eb91f19f";  
+    var searchUrl = "https://www.omdbapi.com/?apikey=" + movieKey + "&s=" + keyword + "&plot=full&r=json";
+
+		$.ajax({
+      url: searchUrl,
+      method: GET
+    }).then(function(response){
+      console.log(response);
+    });
+		
+		
+		//If movie is animated, don't show it
+
+})
+
+
+
+
+
+// Travel API
+function displayTrip() {
+    var tripKey = "";
+  	var tripURL = "";
+  
+  	$.ajax({
+      url: tripURL,
+      method: GET
+    })
+  
+		
+          
+}
+
+
+// On click a selected movie...
+$(document).on("click", ".movieBtn", displayTrip);
