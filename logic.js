7d73caaa221e4d90953d3b45151e0ae6e1a0264c
Charles Davidson <cddavidson86@yahoo.com>
2019-06-17 11:17:50 -0500

    adding new files for CSS

--- logic.js
+++ logic.js
@@ -2,6 +2,8 @@
 
 var keyword = "";
 
+
+
 // This function searches OMDB for the keyword
 function omdbSearch() {
     //OMDB Api Key
@@ -12,8 +14,28 @@ function omdbSearch() {
         url: searchUrl,
         method: "GET"
     }).then(function (response) {
-        // location = response.data.country;
+        var results = response.data;
         console.log(response);
+
+             
+            var poster = results.poster.fixed_height_still.url;
+            var moviePoster = $("#movie-poster");
+                moviePoster.attr("src", poster);
+
+            var title = results.title;
+            var movieTitle = $("#movie-title").text("Title: " + title);  
+
+            var actors = results.actors;
+            var movieActors = $("movie-actors").text("Actors: " + actors);
+
+            var year = results.year;
+            var movieYear = $("#movie-year").text("Year: " + year);
+
+            $("#movie-row").prepend(moviePoster);
+            $("#movie-row").prepend(movieTitle);
+            $("#movie-row").prepend(movieActors);
+            $("#movie-row").prepend(movieYear);
+
     });
 }
 
@@ -136,6 +158,23 @@ $("#submit-btn").on("click", function () {
 
 
 
+// Reddit API
+function displayTrip() {
+    var movieTitle = keyword;
+    var redditURL = "https://www.reddit.com/search.json?&sort=top&t=all&g=" + movieTitle;
 
-// On click a selected movie...
-// $(document).on("click", ".movieBtn", displayTrip);
+    $.ajax({
+        url: redditURL,
+        method: GET
+    })
+
+
+
+}
+
+
+
+
+
+$(document).on("click", ".movieBtn", displayTrip);
+
