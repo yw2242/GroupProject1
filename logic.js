14638c360bd21b4ccd539393ebfe5152564b62df
Charles Davidson <cddavidson86@yahoo.com>
2020-05-18 19:45:37 -0500

    fixed broken link

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 // Comment everything you code!!
 
 var keyword = "";
-var mainPagePosters = ["The Avengers", "The Lion King", "Crazy Rich Asians", "Gladiator", "I Am Legend", "Lord of the Rings"];
+var mainPagePosters = ["The Avengers", "The Lion King", "Crazy Rich Asians", "Gladiator", "I Am Legend", "shrek"];
 var currentFile = window.location.pathname.split("/").pop();
 
 
